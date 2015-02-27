b9b642fb681f8326d3f03cbf339ad741d98ebf42
KyokuX <developer.tcx@gmail.com>
2015-02-27 11:09:51 +0800

    加入WeakReferenceHandler
    ——升级版本号到1.0.7

---
+++ src/main/java/com/xi47/common/android/os/WeakReferenceHandler.java
@@ -0,0 +1,24 @@
+package com.xi47.common.android.os;
+
+import android.os.Handler;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by X on 15/2/27.
+ */
+public class WeakReferenceHandler<T> extends Handler {
+
+    private final WeakReference<T> mReference;
+
+    public WeakReferenceHandler(T reference) {
+        mReference = new WeakReference<T>(reference);
+    }
+
+    protected final T getReference() {
+        if (mReference == null) {
+            return null;
+        }
+        return mReference.get();
+    }
+}
