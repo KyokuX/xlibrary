54ab54bda05a73459f71fd649ee493364828fccf
KyokuX <developer.tcx@gmail.com>
2015-05-20 13:06:12 +0800

    修改获取格式化时间的方式

---
+++ src/main/java/com/xi47/common/java/util/DateTimeUtils.java
@@ -0,0 +1,43 @@
+package com.xi47.common.java.util;
+
+import android.text.TextUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.Locale;
+
+/**
+ * Created by X on 15/5/5.
+ */
+public class DateTimeUtils {
+
+    public static final String TAG_HOUR = "%h";
+    public static final String TAG_MINUTE = "%m";
+    public static final String TAG_SECOND = "%s";
+    public static final String TAG_MILLISECOND = "%ms";
+
+    public static String formatDuration(long duration, String format) {
+        if (TextUtils.isEmpty(format) || duration < 0) {
+            return format;
+        }
+        String formatedString = new String(format);
+        long restDuration = duration;
+        if (formatedString.indexOf(TAG_HOUR) > -1) {
+            long hour = restDuration / 1000 / 60 / 60;
+            formatedString.replaceFirst(TAG_HOUR, String.valueOf(hour));
+            restDuration -= hour * 1000 *60 * 60;
+        } else if (formatedString.indexOf(TAG_MINUTE) > -1) {
+            long minute = restDuration / 1000 / 60;
+            formatedString.replaceFirst(TAG_MINUTE, String.valueOf(minute));
+            restDuration -= minute * 1000 * 60;
+        } else if (formatedString.indexOf(TAG_SECOND) > -1) {
+            long second = restDuration / 1000;
+            formatedString.replaceFirst(TAG_SECOND, String.valueOf(second));
+            restDuration -= second * 1000;
+        } else if (formatedString.indexOf(TAG_MILLISECOND) > -1) {
+            formatedString.replaceFirst(TAG_MILLISECOND, String.valueOf(restDuration));
+        }
+        return formatedString;
+    }
+}
